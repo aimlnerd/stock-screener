d00f047a62cbece6d82602815fc29d16d74a5241
deepak <deepakwithyou@gmail.com>
2020-04-18 12:01:21 +0200

    add option to choose ratios in Metric()

--- data/metric.py
+++ data/metric.py
@@ -8,22 +8,24 @@ LOGGER = logging.getLogger(__name__)
 
 
 class Metric:
-    def __init__(self, ticker, data_api='yahoofinance'):
+    def __init__(self, ticker, data_api='yahoofinance', metric=['balance_sheet']):
         self.ticker = ticker
+        self.metric = metric
         if data_api=='yahoofinance':
             self.data_api = YahooFinance
+        self.metric_fun = {'balance_sheet': self.balance_sheet_metric}
 
-    def latest_qtr_balance_sheet(self, ticker):
+    def __latest_qtr_balance_sheet(self, ticker):
         df = self.data_api(ticker=ticker).quarterly_balance_sheet()
         latest_qtr = max(df.columns)
         return df[latest_qtr]
 
-    def bs_ratios(self):
+    def balance_sheet_metric(self):
         if isinstance(self.ticker, (list)):
             df_master = pd.DataFrame()
             for ticker in self.ticker:
                 df = pd.DataFrame.from_dict({'ticker': [ticker]}, orient='columns')
-                df_bs = self.latest_qtr_balance_sheet(ticker)
+                df_bs = self.__latest_qtr_balance_sheet(ticker)
 
                 latest_qtr_total_assets = self._get_metric(df=df_bs, metric='Total Assets')
                 latest_qtr_total_liabilities = self._get_metric(df=df_bs, metric='Total Liab')
@@ -34,6 +36,13 @@ class Metric:
                 df['latest_qtr_net_debt'] = self._net_debt(debt=latest_qtr_total_short_long_term_debt, cash=latest_qtr_total_cash)
                 df_master = pd.concat([df_master, df], axis=0)
         return df_master
+
+    def __call__(self, *args, **kwargs):
+        df_master = pd.DataFrame()
+        for metric in self.metric:
+            df = self.metric_fun[metric]()
+            df_master = pd.concat([df_master, df], axis=0)
+        return  df_master.reset_index(drop=True)
 
     def _get_metric(self, df, metric):
         return float(df.loc[metric])
@@ -66,11 +75,11 @@ class Metric:
                 df_ticker = df_ticker[['Date', 'Close']]
                 df_ticker['Ticker'] = ticker
                 df = pd.concat([df, df_ticker], axis=0)
-
         return df
 
 
 if __name__ == '__main__':
-    metric = Metric(ticker=['TSLA', 'SABR'], data_api='yahoofinance')
-    metric.bs_ratios()
+    metric = Metric(ticker=['TSLA', 'SABR'], data_api='yahoofinance', metric=['balance_sheet'])
+    df_metric = metric()
+    metric.balance_sheet_metric()
     metric.closing_price()
