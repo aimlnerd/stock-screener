1a17d658f33dc1f8ec41bea9ecdbe96dff80416d
deepak <deepakwithyou@gmail.com>
2020-04-18 12:43:19 +0200

    Add callback to update screener-table

--- dashboard/app.py
+++ dashboard/app.py
@@ -10,26 +10,41 @@ from plot.screener_table import Screener
 
 app = dash.Dash()
 app.layout = html.Div([html.H1(children='Stock Screener'),
-                       html.Div(children='A tool for stock picking.'),
+                       html.H3(children='A tool for stock picking'),
                        html.Label('Input Ticker '),
-                       dcc.Input(id='input_ticker',
+                       dcc.Input(id='input-ticker',
                                  placeholder='Enter list of Ticker',
                                  type='text',
-                                 value="['TSLA', 'APL']"),
-                       dcc.Graph(id='historical_price'),
-                       html.Div(children=[html.H4(children='Top value stocks'),
-                       Screener(ticker=['TSLA', 'AAPL'], data_api='yahoofinance', metric=['balance_sheet']).table()
-                       ])
+                                 value="['TSLA','SABR']"),
+                       html.H2(children='Historical Price'),
+                       dcc.Graph(id='historical-price'),
+                       html.H2(children='Top value stocks'),
+                       html.Div(children=Screener(ticker=['AAPL'], data_api='yahoofinance',
+                                                  metric=['balance_sheet']).table(),
+                                id='screener-table-container')
+
                        ])
 
 
+######### Callbacks ##################
+
+#Update 'historical-price' graph
 @app.callback(
-    Output(component_id='historical_price', component_property='figure'),
-    [Input(component_id='input_ticker', component_property='value')]
+    Output(component_id='historical-price', component_property='figure'),
+    [Input(component_id='input-ticker', component_property='value')]
 )
 def input_to_hist_price(ticker):
     lst_ticker = ast.literal_eval(ticker)
     return Historical(ticker=lst_ticker, data_api='yahoofinance').plot_closing_price()
+
+#Update screener-table
+@app.callback(
+    Output(component_id='screener-table-container', component_property='children'),
+    [Input(component_id='input-ticker', component_property='value')]
+)
+def input_to_screener_table(ticker):
+    lst_ticker = ast.literal_eval(ticker)
+    return Screener(ticker=lst_ticker, data_api='yahoofinance', metric=['balance_sheet']).table()
 
 
 if __name__ == '__main__':
